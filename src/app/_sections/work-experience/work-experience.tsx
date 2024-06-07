fcbf20998f807d5037ea5e5b3accea95c8e33969
Roger Clotet <roger@clotet.dev>
2024-06-07 11:43:44 +0200

    Improve work experience section for small screens

--- src/app/_sections/work-experience/work-experience.tsx
+++ src/app/_sections/work-experience/work-experience.tsx
@@ -7,7 +7,7 @@ import { workExperience } from "./data";
 export default function WorkExperience() {
   return (
     <div>
-      <div className="flex justify-between items-end pb-4 border-[rgb(var(--primary))] border-b-2 mr-4">
+      <div className="flex flex-wrap gap-4 justify-between items-baseline pb-4 border-[rgb(var(--primary))] border-b-2 mr-4">
         <Title>Work Experience</Title>
         <a
           href="https://gitlab.com/rogerclotet/resume/-/raw/master/resume.pdf"
@@ -23,10 +23,10 @@ export default function WorkExperience() {
         <div
           key={idx}
           className={cn(
-            "px-8 py-4 border-[rgb(var(--primary))] border-t-2 mt-[-2px] border-b-2",
+            "px-4 py-2 lg:px-8 lg:py-4 border-[rgb(var(--primary))] border-t-2 mt-[-2px] border-b-2",
             {
-              "pl-4 ml-4": idx % 2 === 0,
-              "pr-4 mr-4": idx % 2 !== 0,
+              "pl-2 ml-2 lg:pl-4 lg:ml-4": idx % 2 === 0,
+              "pr-2 mr-2 lg:pr-4 lg:mr-4": idx % 2 !== 0,
               "rounded-tl-lg": idx % 2 !== 0,
               "rounded-tr-lg": idx % 2 === 0,
               "rounded-bl-lg": idx % 2 !== 0,
