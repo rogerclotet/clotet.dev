220bf6d4ce81af4462ca46851f099d96f1805976
Roger Clotet <roger@clotet.dev>
2024-06-07 17:00:06 +0200

    Header

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+import Logo from "./logo";
+
+export default function Header() {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    function handleScroll() {
+      const displayAt = window.innerHeight / 2;
+      const distanceFromTop = window.scrollY;
+
+      const shouldBeVisible = distanceFromTop >= displayAt;
+
+      if (isVisible !== shouldBeVisible) {
+        setIsVisible(shouldBeVisible);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isVisible]);
+
+  return (
+    <div
+      className={cn(
+        "flex items-center justify-between w-full fixed p-4 transition-all duration-200 ease-in bg-gradient-to-b from-[rgb(var(--background))] to-transparent z-10",
+        {
+          "opacity-100 translate-y-0": isVisible,
+          "opacity-0 translate-y-[-1em]": !isVisible,
+        }
+      )}
+    >
+      <Link href="/" className="text-[rgb(var(--foreground))]">
+        <Logo width={24} height={24} />
+      </Link>
+      <Link href="/" className="text-[rgb(var(--foreground))]">
+        <h1 className="text-xl">Roger Clotet</h1>
+      </Link>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Header from "./_components/header";
 import Revealing from "./_components/revealing";
 import Intro from "./_sections/intro/intro";
 import Outro from "./_sections/outro/outro";
@@ -6,6 +7,7 @@ import WorkExperience from "./_sections/work-experience/work-experience";
 export default function Home() {
   return (
     <>
+      <Header />
       <main className="container mx-auto px-2 lg:px-4 xl:px-8 mb-6">
         <Revealing>
           <Intro />
