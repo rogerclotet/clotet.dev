22c40122492050dc58542e394f3fc4a8e90abb85
Roger Clotet <roger@clotet.dev>
2024-06-17 21:05:01 +0200

    Move blog preview to sections and add margin bottom

--- src/app/_sections/blog-preview.tsx
+++ src/app/_sections/blog-preview.tsx
@@ -1,13 +1,13 @@
 import { getPosts } from "@/lib/blog/posts";
 import Link from "next/link";
+import Title from "../_components/title";
 import PreviewList from "../blog/_components/preview-list";
-import Title from "./title";
 
 export default function BlogPreview() {
   const posts = getPosts().slice(0, 4);
 
   return (
-    <div className="min-h-[70vh] flex flex-col p-2 lg:p-6">
+    <div className="min-h-[70vh] flex flex-col p-2 lg:p-6 mb-20">
       <div className="flex items-baseline justify-between gap-2">
         <Title>Latest articles </Title>
         <Link href="/blog" className="text-2xl monospace">
